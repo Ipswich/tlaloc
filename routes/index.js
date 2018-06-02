c30721f180eed0ec63be7c61abc81ff4ee5f72ff
Ben Steele <bsteele146@gmail.com>
2018-06-02 15:35:50 -0700

    Updated layouts

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+const content = {
+  title: "Tlaloc",
+  Link1: "Overview",
+  Link2: "Link 2",
+  Link3: "Link 3",
+  Link4: "Link 4"
+}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('layout', content);
 });
 
 module.exports = router;
