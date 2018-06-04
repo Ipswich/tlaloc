dca948023cdfdcc2343cfd4500b8eb3b012d3964
Ben Steele <bsteele146@gmail.com>
2018-06-03 22:42:40 -0700

    Updated templates

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//require('bootstrap');
+require('jquery');
+require('popper.js');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
