600ad751ae98549a1bb376ce762512346e5ba9c8
Ben Steele <bsteele146@gmail.com>
2018-07-07 01:27:50 -0700

    Added bootstrap and dependencies to public directories - all css/scripts hosted locally.

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ const content = {
   title: "Tlaloc",
   Link1: "Overview",
   Link2: "Link 2",
+  Link2Sublink1: "Link2Sublink1",
+  Link2Sublink2: "Link2Sublink2",
+  Link2Sublink3: "Link2Sublink3",
+  Link2Sublink4: "Link2Sublink4",
   Link3: "Link 3",
   Link4: "Link 4"
 }
