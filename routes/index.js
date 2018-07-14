1cfb831bcfcfb02c2303e592dc0f357dc5760941
Ben Steele <bsteele146@gmail.com>
2018-07-13 22:25:16 -0700

    Added styling, weather reporting module

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,51 @@
 var express = require('express');
+var weather = require('weather-js');
 var router = express.Router();
 
-/* GET home page. */
-// const content = {
-//   title: "Tlaloc",
-//   Link1: "Overview",
-//   Link2: "Link 2",
-//   Link2Sublink1: "Link2Sublink1",
-//   Link2Sublink2: "Link2Sublink2",
-//   Link2Sublink3: "Link2Sublink3",
-//   Link2Sublink4: "Link2Sublink4",
-//   Link3: "Link 3",
-//   Link4: "Link 4"
-// }
 
-const content = {
-  sidebarTitle: "Tlaloc",
-  sidebarHeading: "Sprinkler Controls",
-  link1: "Overview",
-  link2: "Sprinklers",
-  link2Sublink1: "Sprinkler 1",
-  link2Sublink2: "Sprinkler 2",
-  link2Sublink3: "Sprinkler 3",
-  link2Sublink4: "Sprinkler 4",
-  link3: "Existing Rules",
-  link4: "Create Rule",
-}
 
 router.get('/', function(req, res, next) {
-  res.render('layout', content);
+
+  //Prepare date/time for document
+  var date = new Date();
+  var timeFormatter = function(date){
+    var hours = date.getHours();
+    var hoursMeridian = (hours >= 12) ? "PM" : "AM";
+    var hours = (hours > 12) ? hours - 12 : hours;
+    var minutes = date.getMinutes();
+    var content;
+    if (minutes < 10)
+      minutes = "0" + minutes;
+
+    return hours + ":" + minutes + " " + hoursMeridian;
+  }
+
+  //Prepare weather for document - requires connection (ASYNC)
+  var weatherdata;
+  var error;
+
+  weather.find({search: 'Salem, OR', degreeType: 'F'}, function(err, result) {
+    if(err)
+      error = "Weather could not be loaded."
+    weatherdata = result[0];
+
+      content = {
+      sidebarTitle: "Tlaloc",
+      sidebarHeading: "Sprinkler Controller",
+      link1: "Overview",
+      link2: "Sprinklers",
+      link2Sublink1: "Sprinkler 1",
+      link2Sublink2: "Sprinkler 2",
+      link2Sublink3: "Sprinkler 3",
+      link2Sublink4: "Sprinkler 4",
+      link3: "Existing Rules",
+      link4: "Create Rule",
+      time: date.toDateString() + ", " + timeFormatter(date),
+      weatherdata: weatherdata,
+      error: error
+    }
+    res.render('index', content);
+  });
 });
 
 module.exports = router;
